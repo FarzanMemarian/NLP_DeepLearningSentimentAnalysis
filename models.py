48bd907700352c2c78afba687cd4e29e0c76fc93
frUTCS16 <farzan.memarian@utexas.edu>
2017-10-28 16:53:41 -0500

    implementing LSTM

--- models.py
+++ models.py
@@ -5,6 +5,7 @@ import numpy as np
 import random
 from sentiment_data import *
 from pdb import set_trace
+from random import randint
 
 
 # Returns a new numpy array with the data from np_arr padded to be of length length. If length is less than the
@@ -13,6 +14,127 @@ def pad_to_length(np_arr, length):
     result = np.zeros(length)
     result[0:np_arr.shape[0]] = np_arr
     return result
+
+
+def getTrainBatch():
+    labels = []
+    arr = np.zeros([batchSize, maxSeqLength])
+    for i in range(batchSize):
+        if (i % 2 == 0): 
+            num = randint(1,11499)
+            labels.append([1,0])
+        else:
+            num = randint(13499,24999)
+            labels.append([0,1])
+        arr[i] = ids[num-1:num]
+    return arr, labels
+
+def getTestBatch():
+    labels = []
+    arr = np.zeros([batchSize, maxSeqLength])
+    for i in range(batchSize):
+        num = randint(11499,13499)
+        if (num <= 12499):
+            labels.append([1,0])
+        else:
+            labels.append([0,1])
+        arr[i] = ids[num-1:num]
+    return arr, labels
+
+
+def train_fancy(train_exs, dev_exs, test_exs, word_vectors):
+    # 59 is the max sentence length in the corpus, so let's set this to 60
+    seq_max_len = 60
+    # To get you started off, we'll pad the training input to 60 words to make it a square matrix.
+    train_mat = np.asarray([pad_to_length(np.array(ex.indexed_words), seq_max_len) for ex in train_exs])
+    # Also store the sequence lengths -- this could be useful for training LSTMs
+    train_seq_lens = np.array([len(ex.indexed_words) for ex in train_exs])
+    # Labels
+    train_labels_arr = np.array([ex.label for ex in train_exs])
+
+    maxSeqLength = 10 #Maximum length of sentence
+    numDimensions = 300 #Dimensions for each word vector
+    batchSize = 24
+    lstmUnits = 64
+    numClasses = 2
+    iterations = 100000
+    set_trace()
+
+    # *************************************************
+    # *************************************************
+    # *************************************************
+    # DEFINING THE COMPUTATION GRAPH
+    # Define the core neural network
+    tf.reset_default_graph()
+
+    labels = tf.placeholder(tf.float32, [batchSize, numClasses])
+    input_data = tf.placeholder(tf.int32, [batchSize, maxSeqLength])
+
+    data = tf.Variable(tf.zeros([batchSize, maxSeqLength, numDimensions]),dtype=tf.float32)
+    data = tf.nn.embedding_lookup(wordVectors,input_data)
+
+    lstmCell = tf.contrib.rnn.BasicLSTMCell(lstmUnits)
+    lstmCell = tf.contrib.rnn.DropoutWrapper(cell=lstmCell, output_keep_prob=0.25)
+    value, _ = tf.nn.dynamic_rnn(lstmCell, data, dtype=tf.float32)
+
+    weight = tf.Variable(tf.truncated_normal([lstmUnits, numClasses]))
+    bias = tf.Variable(tf.constant(0.1, shape=[numClasses]))
+    value = tf.transpose(value, [1, 0, 2])
+    last = tf.gather(value, int(value.get_shape()[0]) - 1)
+    prediction = (tf.matmul(last, weight) + bias)
+
+    correctPred = tf.equal(tf.argmax(prediction,1), tf.argmax(labels,1))
+    accuracy = tf.reduce_mean(tf.cast(correctPred, tf.float32))
+
+
+    # load in the network
+    sess = tf.InteractiveSession()
+    saver = tf.train.Saver()
+    saver.restore(sess, tf.train.latest_checkpoint('models'))
+
+
+    # 
+    numFiles = len(train_exs)
+
+    ids = np.zeros((numFiles, maxSeqLength), dtype='int32')
+    fileCounter = 0
+    for pf in positiveFiles:
+       with open(pf, "r") as f:
+           indexCounter = 0
+           line=f.readline()
+           cleanedLine = cleanSentences(line)
+           split = cleanedLine.split()
+           for word in split:
+               try:
+                   ids[fileCounter][indexCounter] = wordsList.index(word)
+               except ValueError:
+                   ids[fileCounter][indexCounter] = 399999 #Vector for unkown words
+               indexCounter = indexCounter + 1
+               if indexCounter >= maxSeqLength:
+                   break
+           fileCounter = fileCounter + 1 
+
+    for nf in negativeFiles:
+       with open(nf, "r") as f:
+           indexCounter = 0
+           line=f.readline()
+           cleanedLine = cleanSentences(line)
+           split = cleanedLine.split()
+           for word in split:
+               try:
+                   ids[fileCounter][indexCounter] = wordsList.index(word)
+               except ValueError:
+                   ids[fileCounter][indexCounter] = 399999 #Vector for unkown words
+               indexCounter = indexCounter + 1
+               if indexCounter >= maxSeqLength:
+                   break
+           fileCounter = fileCounter + 1 
+    #Pass into embedding function and see if it evaluates. 
+
+    np.save('idsMatrix', ids)
+
+
+
 
 
 # Train a feedforward neural network on the given training examples, using dev_exs for development and returning
